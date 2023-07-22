f0c76a49920392f452b6de884c750c2e43a850a1
lpxxn <mi_duo@live.com>
2023-07-22 12:03:32 +0800

    feat: shortcut key

--- config.lua
+++ config.lua
@@ -229,10 +229,14 @@ map["<Leader>tf"] = { "<cmd>HopWord<cr>", "HopWord" }
 -- map["<Leader>th"] = {"<C-w>s", "垂直"} --垂直新增窗口
 
 local keymap = vim.keymap.set
-keymap("n", "gp", "<cmd>Lspsaga peek_definition<CR>")
-keymap("n", "<leader>rn", "<cmd>Lspsaga rename<CR>")
-keymap("n", "<leader>tv", "<C-w>v") --水平新增窗口
-keymap("n", "<leader>th", "<C-w>s") --垂直新增窗口
+keymap("n", "gp", "<cmd>Lspsaga peek_definition<CR>", {desc = "peek_definition"})
+keymap("n", "<leader>rn", "<cmd>Lspsaga rename<CR>", {desc = "rename"})
+keymap("n", "<leader>tv", "<C-w>v", {desc = "vertical split"}) --水平新增窗口
+keymap("n", "<leader>th", "<C-w>s", {desc = "horizontal split"}) --垂直新增窗口
+keymap("n", "<leader>tf", "<cmd>HopWord<cr>", {desc = "HopWord"}) -- 查询
+keymap("n", "<leader>ta", "<cmd>let @+=expand('%:p')<cr>", {desc = "current file path"}) -- 当前 file path
+keymap("n", "<leader>tr", "<cmd>Telescope lsp_references<cr>", {desc = "Telescope references"})
+keymap("n", "<leader>ti", "<cmd>Telescope lsp_implementations<cr>", {desc = "Telescope lsp_implementations"})
 -- 切换 buffer
 -- keymap("n", "<C-S-L>", ":bnext<CR>")
 -- keymap("n", "<C-S-H>", ":bprevious<CR>")
