2d1a10e568ac21f02a7df98cc99a7e43140bf807
lpxxn <mi_duo@live.com>
2023-08-15 16:45:54 +0800

    feat: test config

--- config.lua
+++ config.lua
@@ -191,42 +191,15 @@ local dap = require "dap"
 
 local map = lvim.builtin.which_key.mappings;
 
--- map["<F5>"] = { "<cmd>lua require('dap').toggle_breakpoint()<CR>", "Breakpoint" }
---map['<F5>'] = {"<cmd>lua require'dap'.toggle_breakpoint()<cr>", "Toggle Breakpoint"} 
--- map({ "n", "<F9>", ":lua require('dap').continue()<CR>" })
+-- 下面这几个是能用的
+-- map["<leader>di"] = {"<cmd>lua require('dapui').toggle()<cr>", "dapui toggle" }
+-- map["<leader>ta"] = {"<cmd>let @+=expand('%:p')<cr>", "current file path" }
+-- map["<leader>tr"] = { "<cmd>Telescope lsp_references<cr>", "Telescope references" }
+-- map["<leader>ti"] = { "<cmd>Telescope lsp_implementations<cr>", "Telescope references" }
+-- map["<leader>tf"] = { "<cmd>HopWord<cr>", "HopWord" }
 
--- map({ "n", "<F1>", ":lua require('dap').step_over()<CR>" })
--- map({ "n", "<F2>", ":lua require('dap').step_into()<CR>" })
--- map({ "n", "<F3>", ":lua require('dap').step_out()<CR>" })
-
--- map({ "n", "<Leader>dsc", ":lua require('dap').continue()<CR>" })
--- map({ "n", "<Leader>dsv", ":lua require('dap').step_over()<CR>" })
--- map({ "n", "<Leader>dsi", ":lua require('dap').step_into()<CR>" })
--- map({ "n", "<Leader>dso", ":lua require('dap').step_out()<CR>" })
-
--- map({ "n", "<Leader>dhh", ":lua require('dap.ui.variables').hover()<CR>" })
--- map({ "v", "<Leader>dhv", ":lua require('dap.ui.variables').visual_hover()<CR>" })
-
--- map({ "n", "<Leader>duh", ":lua require('dap.ui.widgets').hover()<CR>" })
--- map({ "n", "<Leader>duf", ":lua local widgets=require('dap.ui.widgets');widgets.centered_float(widgets.scopes)<CR>" })
-
--- map({ "n", "<Leader>dro", ":lua require('dap').repl.open()<CR>" })
--- map({ "n", "<Leader>drl", ":lua require('dap').repl.run_last()<CR>" })
-
--- map({ "n", "<Leader>dbc", ":lua require('dap').set_breakpoint(vim.fn.input('Breakpoint condition: '))<CR>" })
--- map({ "n", "<Leader>dbm", ":lua require('dap').set_breakpoint({ nil, nil, vim.fn.input('Log point message: ') })<CR>" })
--- map({ "n", "<Leader>dbt", ":lua require('dap').toggle_breakpoint()<CR>" })
-
--- map({ "n", "<Leader>dc", ":lua require('dap.ui.variables').scopes()<CR>" })
---map({ "n", "<Leader>di", ":lua require('dapui').toggle()<CR>" })
-map["<Leader>di"] = {"<cmd>lua require('dapui').toggle()<cr>", "dapui toggle" }
-map["<Leader>ta"] = {"<cmd>let @+=expand('%:p')<cr>", "current file path" }
-map["<Leader>tr"] = { "<cmd>Telescope lsp_references<cr>", "Telescope references" }
-map["<Leader>ti"] = { "<cmd>Telescope lsp_implementations<cr>", "Telescope references" }
-map["<Leader>tf"] = { "<cmd>HopWord<cr>", "HopWord" }
-
--- map["<Leader>tv"] = {"<C-w>v", "水平"} --水平新增窗口
--- map["<Leader>th"] = {"<C-w>s", "垂直"} --垂直新增窗口
+-- map["<leader>tv"] = {"<C-w>v", "水平"} --水平新增窗口
+-- map["<leader>th"] = {"<C-w>s", "垂直"} --垂直新增窗口
 
 local keymap = vim.keymap.set
 keymap("n", "gp", "<cmd>Lspsaga peek_definition<CR>", {desc = "peek_definition"})
